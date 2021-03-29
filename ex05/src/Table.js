4fbdfb5ef6e8baa5653fb2e92ee95a80b8bca37b
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-03-29 20:40:03 +0200

    Added ex05

---
+++ ex05/src/Table.js
@@ -0,0 +1,37 @@
+// src/Table.js
+import Table from './src/Table'
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>First name</th>
+                <th>Last name</th>
+            </tr>
+        </thead>
+    )
+
+}
+const TableBody = () => {
+
+    return (
+
+
+        <tbody>
+            <tr>
+                <td>Lary</td>
+                <td>Page</td>
+            </tr>
+            <tr>
+                <td>Ada</td>
+                <td>Lovelace</td>
+            </tr>
+            <tr>
+                <td>Alan</td>
+                <td>Turing</td>
+            </tr>
+        </tbody>
+
+    )
+}
+
+
