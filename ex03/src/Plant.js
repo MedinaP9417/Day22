41ec937a73b65d5fb08805b4f404a0be14d80cf4
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-03-29 17:04:17 +0200

    Added ex03

---
+++ ex03/src/Plant.js
@@ -0,0 +1,17 @@
+// src/Table.js
+
+import React, { Component } from 'react';
+
+
+
+class Plant extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>This is a Plant Component</h1>
+            </div>
+        )
+    }
+}
+
+export default Plant;
