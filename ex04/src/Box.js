602547814f090a2198a52d4f65f3e5c74b82a0e6
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-03-29 19:23:37 +0200

    Added ex04

---
+++ ex04/src/Box.js
@@ -0,0 +1,17 @@
+// src/Table.js
+
+import React, { Component } from 'react';
+
+
+
+class Box extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>This is a Box Component!</h1>
+            </div>
+        )
+    }
+}
+
+export default Box;
